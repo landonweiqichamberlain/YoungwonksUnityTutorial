937dda7d4220c655aeffcea2e7a0ac0c2bfddc06
landonweiqichamberlain <159598212+landonweiqichamberlain@users.noreply.github.com>
2025-09-09 20:45:23 -0700

    Worked on getting the camera to follow the player using target and offset

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;  
+    public Vector3 offset;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
